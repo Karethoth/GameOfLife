6ed1d60f9b27fd286b3c25ff0cebde2e02acad0a
Arttu Multanen <karethoth@gmail.com>
2020-03-18 01:47:59 +0200

    Added a button to constrain new grid size to a square

--- ui/resizedialog.cpp
+++ ui/resizedialog.cpp
@@ -10,16 +10,24 @@ ResizeDialog::ResizeDialog(QWidget *parent, int width, int height) :
     new_width = width;
     new_height = height;
 
-    for(auto &child : this->children())
+    is_grid_size_n_n_constrained = width == height;
+
+    auto *width_input     = this->findChild<QObject*>({"new_width"});
+    auto *height_input    = this->findChild<QObject*>({"new_height"});
+    auto *lock_n_n_button = this->findChild<QObject*>({"lock_n_n"});
+
+    if(width_input)
     {
-        if(child->objectName() == "new_width")
-        {
-            QMetaObject::invokeMethod(child, "setValue", Q_ARG(int, width));
-        }
-        else if(child->objectName() == "new_height")
-        {
-            QMetaObject::invokeMethod(child, "setValue", Q_ARG(int, height));
-        }
+        QMetaObject::invokeMethod(width_input, "setValue", Q_ARG(int, width));
+    }
+
+    if(height_input)
+    {
+        QMetaObject::invokeMethod(height_input, "setValue", Q_ARG(int, height));
+    }
+    if(lock_n_n_button)
+    {
+        QMetaObject::invokeMethod(lock_n_n_button, "setChecked", Q_ARG(bool, is_grid_size_n_n_constrained));
     }
 }
 
@@ -31,9 +39,30 @@ ResizeDialog::~ResizeDialog()
 void ResizeDialog::on_new_width_valueChanged(int arg1)
 {
     new_width = arg1;
+    if(is_grid_size_n_n_constrained)
+    {
+        auto *height_input = this->findChild<QObject*>({"new_height"});
+        if(height_input)
+        {
+            QMetaObject::invokeMethod(height_input, "setValue", Q_ARG(int, arg1));
+        }
+    }
 }
 
 void ResizeDialog::on_new_height_valueChanged(int arg1)
 {
     new_height = arg1;
+    if(is_grid_size_n_n_constrained)
+    {
+        auto *width_input = this->findChild<QObject*>({"new_width"});
+        if(width_input)
+        {
+            QMetaObject::invokeMethod(width_input, "setValue", Q_ARG(int, arg1));
+        }
+    }
+}
+
+void ResizeDialog::on_lock_n_n_toggled(bool checked)
+{
+    is_grid_size_n_n_constrained = checked;
 }
--- ui/resizedialog.h
+++ ui/resizedialog.h
@@ -22,8 +22,12 @@ private slots:
     void on_new_width_valueChanged(int arg1);
     void on_new_height_valueChanged(int arg1);
 
+    void on_lock_n_n_toggled(bool checked);
+
 private:
     Ui::ResizeDialog *ui;
+
+    bool is_grid_size_n_n_constrained;
 };
 
 #endif // RESIZEDIALOG_H
